8f8c3848c4031d7c1571955cd0340e3e3e9e1d5f
Thiago Piazentin <thiagopiazentin3@outlook.com>
2026-05-24 16:08:42 -0300

    feat: implementa a EscrowFactory com todas funcoes

--- contracts/EscrowFactory.sol
+++ contracts/EscrowFactory.sol
@@ -1,8 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import "./Escrow.sol";
+
 contract EscrowFactory {
-    constructor() {
-        
+
+    // Array para salvar os endereços de todos os Escrow criados
+    address[] public todosOsEscrows;
+
+    // Mapping para acahr os escrows de um usuario específico
+    mapping(address => address[]) public escrowUsuario;
+
+    function criaEscrow(address _prestador, uint256 _valor) public {
+        address contratanteAtual = msg.sender;
+        Escrow escrow = new Escrow(contratanteAtual, _prestador, _valor);
+
+        // pega o endereço do escrow que acabamos de criar
+        address enderecoEscrow = address(escrow);
+
+        // adiciona o endereço do escrow na lista global (todosOsEscrows)
+        todosOsEscrows.push(enderecoEscrow);
+
+        // salva na lista específica de quem criou (contratante)
+        escrowUsuario[msg.sender].push(enderecoEscrow);
+
+        // salva na lista específica de quem vai receber (prestador)
+        escrowUsuario[_prestador].push(enderecoEscrow);
+    }
+
+    // retorna todos os escrows criados na plataforma
+    function exibirTodosEscrow() public view returns (address[] memory) {
+        return todosOsEscrows;
+    }
+
+    // retorna apenas o(s) escrow do usuario passado pelo paramêtro
+    function exibirEscrowUsuarioEspecifico(address _usuario) public view returns (address[] memory) {
+        return escrowUsuario[_usuario];
     }
 }
